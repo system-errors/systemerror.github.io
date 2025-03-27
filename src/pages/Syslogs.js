f5549a38dce4242eadaf38c2817e6c26d2ab713d
system-errors <systemerror@proton.me>
2025-03-27 16:39:23 -0400

    chore: new syslog

--- src/pages/Syslogs.js
+++ src/pages/Syslogs.js
@@ -8,7 +8,12 @@ const Syslogs = () => {
       </p>
 
       <div className="log-item">
-        <span>% SYSTEM ERROR #1 [2025-03-26] </span>
+        <span>% SYSTEM ERROR #2 [THE GUILT OF GOLD][2025-03-27] </span>
+        <a className="warning" href="images/SE2.jpeg" target="_blank" rel="noopener noreferrer">(View Log)</a>
+      </div>
+
+      <div className="log-item">
+        <span>% SYSTEM ERROR #1 [EYES CLOSED][2025-03-26] </span>
         <a className="warning" href="images/SE1.jpeg" target="_blank" rel="noopener noreferrer">(View Log)</a>
       </div>
 
